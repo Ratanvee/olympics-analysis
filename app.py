b883a4b5be4b04555f6619e2e320b26b93a614e8
Ratanveer singh <ratanveersingh892@gmail.com>
2024-09-07 21:30:13 +0530

    initial commit

--- app.py
+++ app.py
@@ -235,4 +235,6 @@ async def heatmap_data(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    # uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))  # Use PORT env variable, default to 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
